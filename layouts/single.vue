f7d2bc8bad902490ebca203a5c8449ce503a4cdc
Koyo Kisu <koyokisu@gmail.com>
2020-03-01 15:12:16 +0900

    add layouts single.vue

---
+++ layouts/single.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>single layouts</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <nuxt-link to="/">トップへ戻る</nuxt-link>
+    <hr>
+    <footer>footer</footer> 
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue';
+
+export default {
+  components: {
+    AppNavigation,
+  }
+}
+</script>
+
+<style>
+
+</style>
